6cbac806f9fe0a5891989b0442cff375692a9d32
Blockmaster2706 <blockyork@live.de>
2025-04-03 20:34:17 +0200

    Disable Webkit DMABUF Renderer to fix issue on Fedora

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,5 +1,6 @@
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
+    std::env::set_var("WEBKIT_DISABLE_DMABUF_RENDERER", "1"); // Disable dmabuf renderer for WebKitGTK so that the program starts correctly on all Linux Distros including Fedora
     tauri::Builder::default()
         .setup(|app| {
             if cfg!(debug_assertions) {
