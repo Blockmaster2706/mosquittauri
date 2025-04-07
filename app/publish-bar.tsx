2cf838875a479b284b9a55809f9ef7a06ae01903
Blockmaster2706 <blockyork@live.de>
2025-04-07 18:59:00 +0200

    Change Publish Text Field Placeholder when not connected to Broker

--- app/publish-bar.tsx
+++ app/publish-bar.tsx
@@ -35,7 +35,7 @@ export default function PublishBar({topic, enabled}: PublishBarProps) {
         onKeyDown={handleKeyDown}
         value={inputValue} // Bind the value to the state
         onChange={handleChange} // Update state on input change
-        placeholder="Type here to Publish"
+        placeholder={enabled ? "Type here to Publish" : "Connect to Broker to Publish"}
         className={"w-full mt-auto bg-transparent text-base text-[--white] border-b-[2px] border-white/50 outline-none transition-opacity duration-300 placeholder:text-white/50 focus:opacity-100 focus:border-[var(--accent)] " + (enabled ? "" : "cursor-not-allowed")}
         ></input>
     </div>
