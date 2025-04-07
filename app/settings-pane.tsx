e1b988aaf9903dd6447f1059356ee5af1e1b6a1c
Blockmaster2706 <leon@faerber.dev>
2025-04-07 10:29:11 +0200

    Make connected state mockup

--- app/settings-pane.tsx
+++ app/settings-pane.tsx
@@ -1,6 +1,14 @@
+'use client'
+
+import { useState } from "react";
+
 export default function SettingsPage() {
     const input_classname = "w-[calc(100%-10px)] bg-transparent text-base text-[--white] border-b-[2px] border-white/50 outline-none transition-opacity duration-300 placeholder:text-white/50 focus:opacity-100 focus:border-[var(--accent)]";
     
+    const [connected, setConnected] = useState(false)
+
+    let connected_label = connected ? <label className="text-green-500 pl-2 pb-5">Connected</label> : <label className="text-red-500 pl-2 pb-5">Disconnected</label>
+
     return (
         <div className="absolute left-0 top-0 bg-neutral-800 h-full w-[15%] pt-2 pl-2">
             <label className="w-full accent-text flex justify-center content-center align-middle">Mosquittauri</label>
@@ -13,8 +21,8 @@ export default function SettingsPage() {
 
             <div className="absolute bottom-5 w-full">
 
-                <label className="w-full flex pt-5">Status: <label className="text-green-500 pl-2 pb-5">Connected</label></label>
-                <button className="w-[calc(100%-20px)] h-10 bg-[var(--accent)] text-black border-2 hover:bg-neutral-800 hover:border-[var(--accent)] hover:text-[var(--accent)] duration-100">Connect</button>
+                <label className="w-full flex pt-5">Status: {connected_label}</label>
+                <button onClick={() => {setConnected(!connected)}} className="w-[calc(100%-20px)] h-10 bg-[var(--accent)] text-black border-2 hover:bg-neutral-800 hover:border-[var(--accent)] hover:text-[var(--accent)] duration-100">Connect</button>
             </div>
         </div>
     )
