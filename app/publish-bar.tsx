eaff035316ced4f03ed094630a9782f079393264
Blockmaster2706 <leon@faerber.dev>
2025-04-08 13:05:47 +0200

    Use correct Gray in Input

--- app/publish-bar.tsx
+++ app/publish-bar.tsx
@@ -41,7 +41,7 @@ export default function PublishBar({topicList, enabled}: PublishBarProps) {
         value={inputValue} // Bind the value to the state
         onChange={handleChange} // Update state on input change
         placeholder={enabled ? topic === null ? "Please select a Topic first" : "Type here to Publish" : "Connect to Broker to Publish"}
-        className={"w-full mt-auto bg-transparent text-base text-[--white] border-b-[2px] border-white/50 outline-none transition-opacity duration-300 placeholder:text-white/50 focus:opacity-100 focus:border-[var(--accent)] " + ((enabled && topic) ? "" : "cursor-not-allowed")}
+        className={"w-full mt-auto bg-transparent text-base text-[--white] border-b-[2px] border-neutral-500 outline-none transition-opacity duration-300 placeholder:text-neutral-500 focus:opacity-100 focus:border-[var(--accent)] " + ((enabled && topic) ? "" : "cursor-not-allowed")}
         ></input>
         <PaginatedDropdown options={topicList} onChange={(topic) => {setTopic(topic)}}></PaginatedDropdown>
     </div>
