b487fbd2981fb119ec056677d2c70b16cb6a5ff3
LeSnake04 <dev.lesnake@posteo.de>
2025-06-07 15:06:43 +0200

    rework mqtt (WIP)

--- src-tauri/src/test/mqtt.rs
+++ src-tauri/src/test/mqtt.rs
@@ -1,12 +1,16 @@
 #![cfg(test)]
-use std::{thread::sleep, time::Duration};
+use std::{
+    thread::{self, sleep},
+    time::Duration,
+};
 
 use anyhow::Result;
 use ntest::timeout;
-use tauri::async_runtime::{block_on, channel, spawn};
+use tauri::async_runtime::block_on;
 use test_context::{test_context, TestContext};
 
 use crate::{
+    ipc::event::MqttSendEvent,
     model::{Message, Server},
     mqtt::MqttPool,
 };
@@ -33,32 +37,37 @@ impl TestContext for TestMosquitto {
 
 #[test_context(TestMosquitto)]
 // This test may soft lock on fail
-#[timeout(30_000)]
+#[timeout(60_000)]
 #[test]
 fn test_mosquitto_msqttest(context: &mut TestMosquitto) -> Result<()> {
     super::init_loger();
-    log::info!("create channel");
-    let (message_sender, mut message_receiver) = channel::<Vec<Message>>(32);
+    log::info!("create pool");
+    let (mut pool, msg_sender, msg_receiver) = MqttPool::new(context.server.get_mqtt_options());
     log::info!("listen for messages to print");
-    let message_out_handle = spawn(async move {
-        while let Some(msgs) = message_receiver.recv().await {
-            let payloads: Vec<&str> = msgs.iter().map(|msg| msg.payload()).collect();
+    let message_out_handle = thread::spawn(move || match msg_receiver.recv() {
+        Ok(messages) => {
+            let payloads: Vec<&str> = messages.iter().map(Message::payload).collect();
             println!("Mqtt Messages: {payloads:#?}")
         }
+        Err(e) => {
+            log::warn!("Failed to recieve message to send: {e}");
+        }
     });
-    log::info!("create pool");
-    let mut pool = MqttPool::new(context.server.get_mqtt_options(), message_sender);
     log::info!("subscibe to msqt_test");
     pool.add_subscriber("msqt_test")?;
-    log::info!("wait 10 sec");
-    sleep(Duration::from_secs(10));
+
+    log::info!("send test message");
+    msg_sender.send(MqttSendEvent::new("msqt_test", "Hallo"))?;
+
+    log::info!("wait");
+    sleep(Duration::from_secs(30));
     log::info!("disconnect");
     pool.disconnect();
     log::info!("wait for disconnect");
     block_on(async move {
         message_out_handle
-            .await
-            .expect("Failed to parse message handle");
+            .join()
+            .expect("Failed to wait for message thread");
     });
     Ok(())
 }
