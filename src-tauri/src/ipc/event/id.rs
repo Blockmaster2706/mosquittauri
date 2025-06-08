c3e2f979bb44f9f0694da7443cc7c2bccc48b066
Blockmaster2706 <blockyork@live.de>
2025-06-08 19:02:40 +0200

    Rename topic-pull back to mqtt-pull

--- src-tauri/src/ipc/event/id.rs
+++ src-tauri/src/ipc/event/id.rs
@@ -11,5 +11,5 @@ pub const MQTT_CONNECT: &str = "mqtt-connect";
 pub const MQTT_DISCONNECT: &str = "mqtt-disconnect";
 pub const MQTT_DISCONNECT_REQUEST: &str = "mqtt-disconnect-request";
 pub const MQTT_SEND: &str = "mqtt-send";
-pub const MQTT_PULL: &str = "topic-pull";
+pub const MQTT_PULL: &str = "mqtt-pull";
 pub const MQTT_ERROR: &str = "topic-error";
