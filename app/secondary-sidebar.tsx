39bb4a79179038150393f65fcda48c1ad622f009
Blockmaster2706 <blockyork@live.de>
2025-06-08 19:29:08 +0200

    Make Error "Notification"

--- app/secondary-sidebar.tsx
+++ app/secondary-sidebar.tsx
@@ -20,6 +20,10 @@ interface PaneSwitcherProps {
 	sendButtonEnabled: boolean;
 	autoScrollingDisabled: boolean;
 	setAutoScrollDisabled: Dispatch<SetStateAction<boolean>>;
+	errorCount: number;
+	warningCount: number;
+	setErrorCount: Dispatch<SetStateAction<number>>;
+	setWarningCount: Dispatch<SetStateAction<number>>;
 }
 
 export default function SecondarySidebar({
@@ -31,6 +35,10 @@ export default function SecondarySidebar({
 	sendButtonEnabled,
 	autoScrollingDisabled,
 	setAutoScrollDisabled,
+	errorCount,
+	warningCount,
+	setErrorCount,
+	setWarningCount,
 }: PaneSwitcherProps) {
 	const button_classname =
 		"mt-auto bottom-5 cursor-pointer bg-gray80 w-12 h-12 border-gray80 border-none rounded-full text-[var(--accent)] flex justify-center items-center ";
@@ -65,11 +73,26 @@ export default function SecondarySidebar({
 			) : (
 				<button
 					className={button_classname}
-					onClick={() => setShowingLogs(!isShowingLogs)}
+					onClick={() => {
+						setErrorCount(0);
+						setWarningCount(0);
+						setShowingLogs(!isShowingLogs);
+					}}
 					title="Switch to Logs View"
 				>
 					<MQTTIcon className="size-7" />
 				</button>
+			)}
+			{errorCount > 0 ? (
+				<label className={"text-xs mt-1 text-red-500"}>
+					{errorCount} Errors
+				</label>
+			) : warningCount > 0 ? (
+				<label className={"text-xs mt-1 text-yellow-500"}>
+					{warningCount} Warnings
+				</label>
+			) : (
+				<label className={"text-xs mt-1 "}>0 Errors</label>
 			)}
 			<button
 				disabled={disabled_condition}
