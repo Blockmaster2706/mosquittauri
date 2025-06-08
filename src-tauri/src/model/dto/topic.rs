72772c7cebd17388f43c6118c2039cfb4d5540d9
zk-pre <zk-preX@protonmail.com>
2025-06-08 20:43:07 +0200

    Add some prototype queries

--- src-tauri/src/model/dto/topic.rs
+++ src-tauri/src/model/dto/topic.rs
@@ -2,7 +2,7 @@ use serde::{Deserialize, Serialize};
 
 use super::MsqtDto;
 
-#[derive(Serialize, Deserialize, Clone, Debug)]
+#[derive(Serialize, Deserialize, Clone, Debug, sqlx::FromRow)]
 pub struct Topic {
     pub(in crate::model) id: u64,
     pub(in crate::model) fk_server_id: u64,
