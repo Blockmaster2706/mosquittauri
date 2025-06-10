c7f6653c63c7cfbfc13a4e1abf01b536e265908e
zk-pre <zk-pre@protonmail.com>
2025-06-10 09:48:59 +0200

    implement more sql queries in server, topic

--- src-tauri/src/model/dao/topic.rs
+++ src-tauri/src/model/dao/topic.rs
@@ -1,23 +1,57 @@
 use anyhow::{Context, Result};
+use sqlx::query;
 
 use super::MsqtDao;
 use crate::{
     model::{Session, Topic},
-    utils::JsonStorageLock,
+    utils::{JsonStorageLock, POOL},
 };
 
 static STORAGE: JsonStorageLock<Topic> = JsonStorageLock::new("topic");
 
+macro_rules! topic_from_record {
+    ($record: expr) => {
+        Self {
+            id: $record.id as u32,
+            fk_server_id: $record.fk_server_id as u32,
+            name: $record.name,
+            enabled: $record.enabled,
+        }
+    };
+}
+
 impl MsqtDao for Topic {
     async fn find_all() -> Result<Vec<Topic>> {
-        STORAGE
-            .get()?
-            .find_all()
-            .context("Failed to get full server list")
+        log::info!("getting all topics");
+        let pool = POOL.get().await;
+        let topics = query!(
+            r#"
+            SELECT * FROM Topic
+            "#,
+        )
+        .fetch_all(&*pool)
+        .await?
+        .into_iter()
+        .map(|record| topic_from_record!(record))
+        .collect();
+        Ok(topics)
     }
-    // SELECT * FROM Topic;
     async fn find_by_id(id: u32) -> Result<Self> {
-        todo!()
+        log::info!("find topic by id");
+        let pool = POOL.get().await;
+        let topics = query!(
+            r#"
+            SELECT *
+            FROM Topic
+            WHERE id = ?
+            "#,
+            id
+        )
+        .fetch_optional(&*pool)
+        .await?
+        .map(|record| topic_from_record!(record))
+        .context(format!("Unable to retrieve Server by id: {}", id))?;
+        Ok(topics)
     }
 }
 
@@ -25,56 +59,77 @@ impl Topic {
     pub async fn try_new(server_id: u32, name: impl Into<String>) -> Result<Self> {
         let name = name.into();
         log::info!("adding topic {name}");
-        let topic = Self {
-            id: STORAGE.get()?.gen_id()?,
-            fk_server_id: server_id,
-            name,
-            enabled: false,
-        };
-        STORAGE.get_mut()?.insert(topic.clone())?;
-        Ok(topic)
+        let pool = POOL.get().await;
+        let record = query!(
+            r#"
+            INSERT INTO Topic (fk_server_id, name, enabled)
+            VALUES (?, ?, 0)
+            "#,
+            server_id,
+            name
+        )
+        .fetch_one(&*pool)
+        .await?;
+        Ok(topic_from_record!(record))
     }
-    /*
-     * INSERT INTO Topic (id, fk_server_id, name, enabled)
-     * VALUES ({get_id()}, {server_id}, {name}, 0);
-     */
 
     pub async fn update(id: u32, name: impl Into<String>) -> Result<()> {
         let name = name.into();
         log::info!("updating topic {name}");
-        STORAGE.get_mut()?.edit(id, |topic| topic.name = name)
+        let pool = POOL.get().await;
+        query!(
+            r#"
+            UPDATE Topic
+            SET name = ?
+            WHERE id = ?
+            "#,
+            name,
+            id
+        )
+        .execute(&*pool)
+        .await?;
+        Ok(())
     }
-    /*
-     * UPDATE Topic
-     * SET name = {name}
-     * WHERE id = {id};
-     */
 
     pub async fn set_enabled(topic_id: u32, enabled: bool) -> Result<()> {
-        STORAGE.get_mut()?.edit(topic_id, |topic| {
-            log::info!("setting topic {} state to {}", topic.name, enabled);
-            topic.enabled = enabled
-        })
+        //log::info!(format!("setting topic with id {} state to {}", topic_id, enabled));
+        let pool = POOL.get().await;
+        //let enabled = enabled as i32;
+        query!(
+            r#"
+            UPDATE Topic
+            SET enabled = ?
+            WHERE id = ?
+            "#,
+            enabled,
+            topic_id
+        )
+        .execute(&*pool)
+        .await?;
+        Ok(())
     }
-    /*
-     * UPDATE Topic
-     * SET enabled = {enabled}
-     * WHERE id = {id};
-     */
 
     #[allow(dead_code)]
     pub async fn find_enabled_by_server(server_id: u32) -> Result<Vec<Topic>> {
         log::info!("getting enabled topics for server with id");
-        Ok(Self::find_all()
-            .await?
-            .into_iter()
-            .filter(|t: &Topic| t.enabled && t.server_id() == server_id)
-            .collect())
+        let pool = POOL.get().await;
+        let topics = query!(
+            r#"
+            SELECT * FROM Topic
+            WHERE fk_server_id = ? AND enabled = TRUE;
+            "#,
+            server_id
+        )
+        .fetch_optional(&*pool)
+        .await?
+        .map(|record| topic_from_record!(record))
+        .context(format!(
+            "Unable to retrieve Topics for Server with id: {}",
+            server_id
+        ))?;
+        todo!();
+        //Ok(topics)
     }
-    /*
-     * SELECT * FROM Topic
-     * WHERE fk_server_id = {server_id} AND enabled = 1;
-     */
 
     pub async fn find_by_server(server_id: u32) -> Result<Vec<Topic>> {
         log::info!("getting topics for server with id");
@@ -110,10 +165,15 @@ impl Topic {
 
     pub async fn delete(id: u32) -> Result<()> {
         log::info!("deleting topic with id {id}");
-        STORAGE.get_mut()?.delete(id)
+        let pool = POOL.get().await;
+        query!(
+            r#"
+            DELETE FROM Topic WHERE id = ?
+            "#,
+            id
+        )
+        .execute(&*pool)
+        .await?;
+        Ok(())
     }
-    /*
-     * DELETE FROM Topic
-     * WHERE id = {id};
-     */
 }
