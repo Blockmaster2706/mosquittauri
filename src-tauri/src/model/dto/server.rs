81953a790079e605c7ff2802e3d52bc237e20bb5
LeSnake04 <dev.lesnake@posteo.de>
2025-06-10 10:40:00 +0200

    fixed Server::find_by_name query

--- src-tauri/src/model/dto/server.rs
+++ src-tauri/src/model/dto/server.rs
@@ -3,7 +3,7 @@ use serde::{Deserialize, Serialize};
 
 use super::MsqtDto;
 
-#[derive(Serialize, Deserialize, Clone, Debug, sqlx::FromRow)]
+#[derive(Serialize, Deserialize, Clone, Debug, sqlx::FromRow, Eq, PartialEq, PartialOrd, Ord)]
 #[serde(rename_all = "camelCase")]
 pub struct Server {
     pub(in crate::model) id: u32,
