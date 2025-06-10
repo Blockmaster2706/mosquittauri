e902e76ce704a85502e4d1b96b60eab9f8b3dd55
LeSnake04 <dev.lesnake@posteo.de>
2025-06-10 21:06:09 +0200

    removed json storage

--- src-tauri/src/utils/mod.rs
+++ src-tauri/src/utils/mod.rs
@@ -1,10 +1,7 @@
-mod json_storage;
 mod lock;
 mod log;
 mod sqlite_storage;
 
-#[allow(unused_imports)]
-pub use json_storage::{JsonStorage, JsonStorageLock, JsonStorageRead, JsonStorageWrite};
 #[allow(unused_imports)]
 pub use lock::Lock;
 #[allow(unused_imports)]
