9e49aacce9f9d3ae7676ffba84465f8a9d2a8a1b
David Volkers <david.volkers@gwdg.de>
2025-06-10 16:25:53 +0200

    fix queries

--- src-tauri/src/model/dao/session.rs
+++ src-tauri/src/model/dao/session.rs
@@ -1,20 +1,18 @@
 use anyhow::{Context, Result};
+use sqlx::query;
 
-use crate::{
-    model::Session,
-    utils::{JsonStorageLock, POOL},
-};
+use crate::{model::Session, utils::POOL};
 
 use super::MsqtDao;
-
-static STORAGE: JsonStorageLock<Session> = JsonStorageLock::new("session");
 
 macro_rules! session_from_record {
     ($record: expr) => {
         Self {
             id: $record.id as u32,
-            fk_selected_server_id: $record.fk_selected_server_id as u32,
-            all_topics: $record.all_topics,
+            fk_selected_server_id: $record
+                .fk_selected_server_id
+                .map(|server_id| server_id as u32),
+            listen_all_topics: $record.listen_all_topics,
             connected: $record.connected,
         }
     };
@@ -55,22 +53,17 @@ impl Session {
             return Ok(session);
         }
         let session = Self::default();
-        let id = session.id;
-        let fk_selected_server_id = session.fk_selected_server_id;
-        let all_topics = session.all_topics;
-        let connected = session.connected;
         let pool = POOL.get().await;
-        log::info!("adding server {name}");
         let record = query!(
             r#"
-            INSERT INTO Session (id, fk_selected_server_id, all_topics, connected)
-            VALUES (?, ?, ?, ?, ?)
+            INSERT INTO Session (id, fk_selected_server_id, listen_all_topics, connected)
+            VALUES (?, ?, ?, ?)
             RETURNING *;
             "#,
-            id,
-            fk_selected_server_id,
-            all_topics,
-            connected
+            session.id,
+            session.fk_selected_server_id,
+            session.listen_all_topics,
+            session.connected
         )
         .fetch_one(&*pool)
         .await?;
@@ -79,18 +72,21 @@ impl Session {
     pub async fn select_server(server_id: u32) -> Result<()> {
         log::info!("find selected server by server_id");
         let pool = POOL.get().await;
-        let sessions = query!(
+        query!(
             r#"
-            SELECT * FROM Session
-            WHERE fk_selected_server_id = ?
+            UPDATE Session
+            SET fk_selected_server_id = ?
+            WHERE id = 0
             "#,
             server_id
         )
-        .fetch_optional(&*pool)
-        .await?
-        .map(|record| session_from_record!(record))
-        .context(format!("Unable to retrieve selected Server by id: {}", id))?;
-        Ok(sessions)
+        .execute(&*pool)
+        .await
+        .context(format!(
+            "Unable to retrieve selected Server by id: {}",
+            server_id
+        ))?;
+        Ok(())
     }
 
     pub async fn set_listen_all_topics(enabled: bool) -> Result<()> {
@@ -98,10 +94,10 @@ impl Session {
         query!(
             r#"
             UPDATE Session
-            SET all_topics = TRUE
-            WHERE id = ?
+            SET listen_all_topics = ?
+            WHERE id = 0
             "#,
-            id
+            enabled
         )
         .execute(&*pool)
         .await?;
