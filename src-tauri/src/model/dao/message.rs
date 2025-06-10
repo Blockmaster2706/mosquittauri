3c35234c912f39fe80e97518860f52cf6dc8e62b
zk-pre <zk-pre@protonmail.com>
2025-06-10 15:03:59 +0200

    implement queries for message and session

--- src-tauri/src/model/dao/message.rs
+++ src-tauri/src/model/dao/message.rs
@@ -7,30 +7,69 @@ use super::MsqtDao;
 
 static STORAGE: JsonStorageLock<Message> = JsonStorageLock::new("message");
 
+macro_rules! message_from_record {
+    ($record: expr) => {
+        Self {
+            id: $record.id as u32,
+            fk_server_id: $record.fk_server_id as u32,
+            topic: $record.topic,
+            payload: $record.payload,
+        }
+    };
+}
+
 impl MsqtDao for Message {
     async fn find_all() -> Result<Vec<Self>> {
-        STORAGE.get()?.find_all()
+        log::info!("getting all messages");
+        let pool = POOL.get().await;
+        let messages = query!(
+            r#"
+            SELECT * FROM Messages
+            "#,
+        )
+        .fetch_all(&*pool)
+        .await?
+        .into_iter()
+        .map(|record| message_from_record!(record))
+        .collect();
+        Ok(messages)
     }
     async fn find_by_id(id: u32) -> Result<Self> {
-        todo!()
+        log::info!("find message by id");
+        let pool = POOL.get().await;
+        let messages = query!(
+            r#"
+            SELECT * FROM Messages
+            WHERE id = ?
+            "#,
+            id
+        )
+        .fetch_optional(&*pool)
+        .await?
+        .map(|record| message_from_record!(record))
+        .context(format!("Unable to retrieve Message by id: {}", id))?;
+        Ok(messages)
     }
-    /*
-     * SELECT * FROM Messages;
-     */
 }
 
 impl Message {
     pub async fn try_new(server_id: u32, topic: String, payload: String) -> Result<Self> {
-        Ok(Self {
-            id: STORAGE.get()?.gen_id()?,
-            fk_server_id: server_id,
+        let timestamp = Local::now().timestamp();
+        let pool = POOL.get().await;
+        log::info!("adding topic {topic}");
+        let record = query!(
+            r#"
+            INSERT INTO Messages (fk_server_id, topic, payload, timestamp)
+            VALUES (?, ?, ?, ?)
+        RETURNING *;
+        "#,
+            server_id,
             topic,
             payload,
-            timestamp: Local::now().timestamp(),
-        })
+            timestamp
+        )
+        .fetch_one(&*pool)
+        .await?;
+        Ok(server_from_record!(record))
     }
 }
-/*
- * INSERT INTO Messages (id, fk_server_id, topic, payload, timestamp)
- * VALUES ({gen_id()}, {server_id}, {topic}, {payload}, {Local::now().timestamp()});
-*/
--- src-tauri/src/model/dao/session.rs
+++ src-tauri/src/model/dao/session.rs
@@ -1,20 +1,51 @@
 use anyhow::{Context, Result};
 
-use crate::{model::Session, utils::JsonStorageLock};
+use crate::{
+    model::Session,
+    utils::{JsonStorageLock, POOL},
+};
 
 use super::MsqtDao;
 
 static STORAGE: JsonStorageLock<Session> = JsonStorageLock::new("session");
 
+macro_rules! session_from_record {
+    ($record: expr) => {
+        Self {
+            id: $record.id as u32,
+            fk_selected_server_id: $record.fk_selected_server_id as u32,
+            all_topics: $record.all_topics,
+            connected: $record.connected,
+        }
+    };
+}
+
 impl MsqtDao for Session {
     async fn find_all() -> Result<Vec<Self>> {
-        STORAGE.get()?.find_all()
+        log::info!("getting all sessions");
+        let pool = POOL.get().await;
+        let sessions = query!(r#"SELECT * FROM Session"#,)
+            .fetch_all(&*pool)
+            .await?
+            .into_iter()
+            .map(|record| session_from_record!(record))
+            .collect();
+        Ok(sessions)
     }
-    /*
-     * SELECT * FROM Session;
-     */
     async fn find_by_id(id: u32) -> Result<Self> {
-        todo!()
+        log::info!("find server by id");
+        let pool = POOL.get().await;
+        let sessions = query!(
+            r#"
+            SELECT * FROM Session WHERE id = ?
+            "#,
+            id
+        )
+        .fetch_optional(&*pool)
+        .await?
+        .map(|record| session_from_record!(record))
+        .context(format!("Unable to retrieve Server by id: {}", id))?;
+        Ok(sessions)
     }
 }
 
@@ -24,36 +55,56 @@ impl Session {
             return Ok(session);
         }
         let session = Self::default();
-        STORAGE.get_mut()?.insert(session.clone())?;
-        Ok(session)
+        let id = session.id;
+        let fk_selected_server_id = session.fk_selected_server_id;
+        let all_topics = session.all_topics;
+        let connected = session.connected;
+        let pool = POOL.get().await;
+        log::info!("adding server {name}");
+        let record = query!(
+            r#"
+            INSERT INTO Session (id, fk_selected_server_id, all_topics, connected)
+            VALUES (?, ?, ?, ?, ?)
+            RETURNING *;
+            "#,
+            id,
+            fk_selected_server_id,
+            all_topics,
+            connected
+        )
+        .fetch_one(&*pool)
+        .await?;
+        Ok(session_from_record!(record))
     }
-    /*
-     * SELECT
-     * INSERT INTO Session (id, fk_server_id, name, )
-     */
     pub async fn select_server(server_id: u32) -> Result<()> {
-        STORAGE
-            .get_mut()?
-            .update(|list| {
-                list[0].fk_selected_server_id = Some(server_id);
-                Ok(())
-            })
-            .context("failed to set selected server id")?;
-        Ok(())
+        log::info!("find selected server by server_id");
+        let pool = POOL.get().await;
+        let sessions = query!(
+            r#"
+            SELECT * FROM Session
+            WHERE fk_selected_server_id = ?
+            "#,
+            server_id
+        )
+        .fetch_optional(&*pool)
+        .await?
+        .map(|record| session_from_record!(record))
+        .context(format!("Unable to retrieve selected Server by id: {}", id))?;
+        Ok(sessions)
     }
-    /*
-     * SELECT * FROM Session
-     * WHERE id = {id};
-     */
 
     pub async fn set_listen_all_topics(enabled: bool) -> Result<()> {
-        STORAGE
-            .get_mut()?
-            .edit(0, |session| session.listen_all_topics = enabled)
+        let pool = POOL.get().await;
+        query!(
+            r#"
+            UPDATE Session
+            SET all_topics = TRUE
+            WHERE id = ?
+            "#,
+            id
+        )
+        .execute(&*pool)
+        .await?;
+        Ok(())
     }
-    /*
-     * UPDATE Session
-     * SET  all_topics = 1
-     * WHERE id = {id};
-     */
 }
